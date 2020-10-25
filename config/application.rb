63d0d27462a183b258d54be423fd58b7414052a2
gucchi-ta <ta.yamaguchi.1225@gmail.com>
2020-10-25 16:37:36 +0900

    ユーザー管理　バリデーションrender時にビューが崩れないように

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,9 @@ module InoteApp
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
+    # バリデーションrender時にビューが崩れないように
+    config.action_view.field_error_proc = Proc.new { |html_tag, instance| html_tag }
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading

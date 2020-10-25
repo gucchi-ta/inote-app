a7c6248a23eea2c51d2182d538f73b11c577662e
gucchi-ta <ta.yamaguchi.1225@gmail.com>
2020-10-25 20:59:11 +0900

    新規ユーザー登録　結合テストコード

---
+++ spec/system/users_spec.rb
@@ -0,0 +1,64 @@
+require 'rails_helper'
+
+def basic_pass(path)
+  username = ENV["BASIC_AUTH_USER"] 
+  password = ENV["BASIC_AUTH_PASSWORD"]
+  visit "http://#{username}:#{password}@#{Capybara.current_session.server.host}:#{Capybara.current_session.server.port}#{path}"
+end
+
+RSpec.describe 'ユーザー新規登録', type: :system do
+  before do
+    @user = FactoryBot.build(:user)
+  end
+  context 'ユーザー新規登録ができるとき' do 
+    it '正しい情報を入力すればユーザー新規登録ができてトップページに移動する' do
+      # トップページに移動する
+      basic_pass root_path
+      # トップページにサインアップページへ遷移するボタンがあることを確認する
+      expect(page).to have_css '#signup_icon'
+      # 新規登録ページへ移動する
+      visit new_user_registration_path
+      # ユーザー情報を入力する
+      fill_in 'nickname', with: @user.nickname
+      fill_in 'email', with: @user.email
+      fill_in 'password', with: @user.password
+      fill_in 'password_confirmation', with: @user.password_confirmation
+      select '1930',from: 'user_birthday_1i'
+      select '12',from: 'user_birthday_2i'
+      select '12',from: 'user_birthday_3i'
+      # サインアップボタンを押すとユーザーモデルのカウントが1上がることを確認する
+      expect{
+        find('input[name="commit"]').click
+      }.to change { User.count }.by(1)
+      # トップページへ遷移する
+      expect(current_path).to eq root_path
+      # サインアップページへ遷移するボタンや、ログインページへ遷移するボタンが表示されていないことを確認する
+      expect(page).to have_no_css '#signup_icon'
+      expect(page).to have_no_css '#login_icon'
+    end
+  end
+  context 'ユーザー新規登録ができないとき' do
+    it '誤った情報ではユーザー新規登録ができずに新規登録ページへ戻ってくる' do
+      # トップページに移動する
+      basic_pass root_path
+      # トップページにサインアップページへ遷移するボタンがあることを確認する
+      expect(page).to have_css '#signup_icon'
+      # 新規登録ページへ移動する
+      visit new_user_registration_path
+      # ユーザー情報を入力する
+      fill_in 'nickname', with: ""
+      fill_in 'email', with: ""
+      fill_in 'password', with: ""
+      fill_in 'password_confirmation', with: ""
+      select '--',from: 'user_birthday_1i'
+      select '--',from: 'user_birthday_2i'
+      select '--',from: 'user_birthday_3i'
+      # サインアップボタンを押してもユーザーモデルのカウントは上がらないことを確認する
+      expect{
+        find('input[name="commit"]').click
+      }.to change { User.count }.by(0)
+      # 新規登録ページへ戻されることを確認する
+      expect(current_path).to eq "/users"
+    end
+  end
+end

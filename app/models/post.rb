6481ae37d989f3cbd6d1f674235b1fd7b6af6a4c
gucchi-ta <ta.yamaguchi.1225@gmail.com>
2020-10-27 23:50:49 +0900

    検索機能　モデル

--- app/models/post.rb
+++ app/models/post.rb
@@ -7,4 +7,20 @@ class Post < ApplicationRecord
 
   # 空の値を登録できないようにバリデーション
   validates :image, presence: true
+
+  def self.search(search)
+    if search != ""
+      Post.where('grobal LIKE ? and memo LIKE(?)', "1", "%#{search}%").order('created_at DESC')
+    else
+      Post.where('grobal LIKE ?', "1").order('created_at DESC')
+    end
+  end
+
+  def self.my_search(search)
+    if search != ""
+      Post.where('memo LIKE(?)', "%#{search}%").order('created_at DESC')
+    else
+      Post.all.order('created_at DESC')
+    end
+  end
 end

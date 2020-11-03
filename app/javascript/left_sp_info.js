d30165a0799c67d0a4e056f2972fe6f63c49b599
gucchi-ta <ta.yamaguchi.1225@gmail.com>
2020-11-04 08:43:02 +0900

    ヘッダー　.left-sp-nonlog-info-box　その他クラス名　変更

--- app/javascript/left_sp_info.js
+++ app/javascript/left_sp_info.js
@@ -1,15 +1,24 @@
-// 「.arrow_bk-icon」をクリックすると交互に「style="display: block"」と「style="display: none"」を繰り返す
+// 「.arrow-bk-icon」をクリックすると交互に「style="display: block"」と「style="display: none"」を繰り返す
 $(function() { 
   $(window).on('load', function() {
-      var clicked = false;
-      $('.arrow_bk-icon').on('click', function() {
-        const leftSpInfoBox = document.getElementsByClassName("left-sp-info-box");
-        clicked = !clicked;
-        if (clicked) {
-          leftSpInfoBox[0].setAttribute("style", "display: block");
-        }else {
-          leftSpInfoBox[0].setAttribute("style", "display: none");
-        }
-      });
+    var clicked = false;
+    $('.arrow-bk-icon').on('click', function() {
+      const leftSpInfoBox = document.getElementsByClassName("left-sp-info-box");
+      clicked = !clicked;
+      if (clicked) {
+        leftSpInfoBox[0].setAttribute("style", "display: block");
+      }else {
+        leftSpInfoBox[0].setAttribute("style", "display: none");
+      }
+    });
+    $('.arrow-bk-nonlog-icon').on('click', function() {
+      const leftSpNonlogInfoBox = document.getElementsByClassName("left-sp-nonlog-info-box");
+      clicked = !clicked;
+      if (clicked) {
+        leftSpNonlogInfoBox[0].setAttribute("style", "display: block");
+      }else {
+        leftSpNonlogInfoBox[0].setAttribute("style", "display: none");
+      }
+    });
   });
 });

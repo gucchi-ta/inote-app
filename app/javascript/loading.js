31536c10de72e424859fbf05801886236da44e4a
gucchi-ta <ta.yamaguchi.1225@gmail.com>
2020-11-05 21:55:05 +0900

    ローディングアニメーション　表示時間の制限を削除

--- app/javascript/loading.js
+++ app/javascript/loading.js
@@ -7,7 +7,7 @@ $(function(){
 	});
 
 	//ページの読み込みが完了してなくても3秒後にアニメーションを非表示にする
-	setTimeout(function(){
-		loader.fadeOut();
-	},3000);
+	// setTimeout(function(){
+	// 	loader.fadeOut();
+	// },3000);
 });
